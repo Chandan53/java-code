b3fde94f7b424acbad003a53fa0ddf9b6a06688a
chandan <chandankumar936672@gmail.com>
2025-06-09 23:59:16 +0530

    java code 1

--- array1.java
+++ array1.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class array1 {
     public static void main(String args[]){
-        int array1[]=new int[5];
-        array1[0]=1;
-        array1[1]=2;
-        array1[2]=3;
-        array1[3]=4;
-        array1[4]=5;
+        int marks[]=new int[5];
+        Scanner sc=new Scanner (System.in);
+         marks[0]= sc.nextInt();
+         marks[1]=sc.nextInt();
+         marks[2]=sc.nextInt();
+
+         System.out.println("phy = " + marks[0]);
+         System.out.println("maths = " + marks[1]);
+         System.out.println("chem = " + marks[2]);
+
+
+     
+     
 
 
 
